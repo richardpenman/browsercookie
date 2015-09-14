9a8401318a9b3d84be20c7bc154c5d11aca64b85
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2015-09-14 08:59:34 +0100

    migrated browser_cookie to browsercookie to follow pep8

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ if sys.version_info.major >= 3:
     sys.exit(1)
 
 setup(
-    name='browser-cookie', 
-    version='0.6',
-    packages=['browser_cookie'],
-    package_dir={'browser_cookie' : '.'}, # look for package contents in current directory
+    name='browsercookie', 
+    version='0.6.1',
+    packages=['browsercookie'],
+    package_dir={'browsercookie' : '.'}, # look for package contents in current directory
     author='Richard Penman',
     author_email='richard@webscraping.com',
     description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
-    url='https://bitbucket.org/richardpenman/browser_cookie',
+    url='https://bitbucket.org/richardpenman/browsercookie',
     install_requires=['pycrypto', 'keyring'],
     license='lgpl'
 )
