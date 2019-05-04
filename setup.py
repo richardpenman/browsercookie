18d9ebc5d35ae842d31b7ae6fddaa00b9391815e
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2019-05-04 15:54:57 -0400

    use maintained crypto dependency

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     author_email='richard@webscraping.com',
     description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
     url='https://bitbucket.org/richardpenman/browsercookie',
-    install_requires=['pycrypto', 'keyring', 'lz4'],
+    install_requires=['pycryptodome', 'keyring', 'lz4'],
     license='lgpl'
 )
