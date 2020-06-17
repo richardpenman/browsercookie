77250f74cdc260d5ed4b8dd349e211806c9bdf0a
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2020-06-17 11:32:03 -0400

    updated setup url

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@ import sys
 import os
 from distutils.core import setup
 
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 setup(
     name='browsercookie', 
-    version='0.7.6',
+    version='0.7.7',
     packages=['browsercookie'],
     package_dir={'browsercookie' : '.'}, # look for package contents in current directory
     author='Richard Penman',
-    author_email='richard@webscraping.com',
+    author_email='richard.penman@gmail.com',
     description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
-    url='https://bitbucket.org/richardpenman/browsercookie',
+    long_description=read('README.rst'),
+    url='https://github.com/richardpenman/browsercookie',
     install_requires=['pycryptodome', 'keyring', 'lz4'],
     license='lgpl'
 )
