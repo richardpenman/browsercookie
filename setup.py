fec9d4f94d9e405759960ea946dea53e0074b1f8
Oldřich Jedlička <oldrich.jedlicka@rohlik.cz>
2023-10-24 21:31:31 +0200

    Fix setup to allow local development
    This structure allows installing with `pip install -e <folder>`.
    
    Signed-off-by: Oldřich Jedlička <oldium.pro@gmail.com>

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     name='browsercookie', 
     version='0.7.7',
     packages=['browsercookie'],
-    package_dir={'browsercookie' : '.'}, # look for package contents in current directory
     author='Richard Penman',
     author_email='richard.penman@gmail.com',
     description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
     long_description=read('README.rst'),
     url='https://github.com/richardpenman/browsercookie',
-    install_requires=['pycryptodome', 'keyring', 'lz4'],
+    install_requires=['pycryptodome', 'keyring', 'lz4',
+                      'pywin32; sys_platform == "win32"'],
     license='lgpl'
 )
