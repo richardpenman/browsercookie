7fa28cedd23a041ee38cdaa085d60b7eeb71b7e8
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2025-02-20 21:33:29 +0800

    bumped pypi version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(filename):
 
 setup(
     name='browsercookie', 
-    version='0.8.0',
+    version='0.8.1',
     python_requires='>=3.4',
     packages=['browsercookie'],
     author='Richard Penman',
